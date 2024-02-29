d1564ccddfa49e0a09e9b0f6d161ee6f4bafe5da
Tsiory <fordevproject@gmail.com>
2024-02-29 15:44:17 +0300

    Dep: Stack navigation

--- App.js
+++ App.js
@@ -1,20 +1,40 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { View, Text, Button } from "react-native";
+import { NavigationContainer, useNavigation } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+
+function HomeScreen(props) {
+  return(
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}} >
+      <Text>Bonjour</Text>
+      <Text>{ JSON.stringify(props.route.params) }</Text>
+      <Button title='Home' onPress={() => (props.navigation.navigate('Home', {id: 1, name: "Laurent"}))} />
+      <Button title='Home' onPress={() => (props.navigation.navigate('Home', {id: 2, name: "Mark"}))} />
     </View>
-  );
-}
+  )
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+function ContactScreen() {
+  const navigation = useNavigation();
+  return(
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}} >
+      <Text>Accueil</Text>
+      <Button title='Home' onPress={() => (navigation.goBack())} />
+    </View>
+  )
+};
+
+
+const Stack = createStackNavigator();
+
+export default function App(){
+  return(
+    <NavigationContainer>
+        <Stack.Navigator>
+            <Stack.Screen name="Home" component={HomeScreen} />
+            <Stack.Screen name="Contact" component={ContactScreen} />
+        </Stack.Navigator>
+    </NavigationContainer>
+  )
+}
