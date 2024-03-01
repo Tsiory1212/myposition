4c78fff09d8dbec30bc275aa3e31d3272c9d0c6e
Tsiory <fordevproject@gmail.com>
2024-03-01 10:51:15 +0300

    LayoutAnimation

--- App.js
+++ App.js
@@ -1,23 +1,53 @@
-import * as React from 'react';
-import { Animated, Text, useWindowDimensions, Easing } from "react-native";
+import react,  {useState} from 'react';
+import { View, Platform, UIManager, LayoutAnimation, StyleSheet, Button } from "react-native";
 
+if (
+  Platform.OS === "android" && 
+  UIManager.setLayoutAnimationEnabledExperimental
+) {
+  UIManager.setLayoutAnimationEnabledExperimental(true)
+}
 
 export default function App(){
-  const windowHeight = Math.round(useWindowDimensions().height);
-  const heightAnimation = React.useRef(new Animated.Value(0)).current
-  
-  React.useEffect(() => {
-    Animated.loop(
-      Animated.timing(heightAnimation, {
-        toValue: windowHeight,
-        duration: 10000,
-        useNativeDriver: false,
-      })).start();
-  }, [heightAnimation, windowHeight])
+  const [boxPosition, setBoxPosition] = useState('left');
+  const togggleBox = () => {
+    LayoutAnimation.configureNext(LayoutAnimation.Presets.linear);
+    setBoxPosition(boxPosition === "left" ? "right" : "left");
+  }
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      alignItems: 'flex-start',
+      justifyContent: 'center'
+    },
+    box: {
+      height: 100,
+      width: 100,
+      borderRadius: 5,
+      margin: 8,
+      backgroundColor: 'blue'
+    },
+    moveRight: {
+      alignSelf: "flex-end",
+      height: 200,
+      width: 200
+    },
+    buttonContainer: {
+      alignSelf: 'center'
+    }
+    
+  })
 
   return(
-      <Animated.View 
-        style={{ backgroundColor: "red", width: "100%", height: heightAnimation}}
-      />
+      <View style={styles.container}>
+        <View style={styles.buttonContainer}>
+          <Button title='déplacer la boite' onPress={togggleBox} />
+        </View>
+        <View style={[styles.box, boxPosition === "left" ? null : styles.moveRight]}>
+
+        </View>
+      </View>
   )
+
+ 
 }
