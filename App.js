2e8a2389638429863924b22bd55142b2988771ca
Tsiory <fordevproject@gmail.com>
2024-03-01 12:51:54 +0300

    API expo et les permissions

--- App.js
+++ App.js
@@ -1,41 +1,43 @@
-import { View, Text, StyleSheet } from "react-native";
-import * as SQL from "expo-sqlite";
-import { useEffect, useState } from "react";
+import { View, Text, StyleSheet, Button } from "react-native";
+import * as Location from "expo-location";
+import { useState } from "react";
 
-const db = SQL.openDatabase('test.db');
 
 export default function App(){
   const styles = StyleSheet.create({
     container: {
+      flex: 1,
       alignItems: 'center',
-      height: 100,
-      justifyContent: 'center'
-    }
+      justifyContent: 'center',
+      padding: 20,
+    },
   })
 
-  const [table, setTable] = useState(null);
-  const [forceUptadeId, forceUpdate] = useForceUpdate();
-  
-  useEffect(() => {
-    db.exec([{sql: "PRAGMA foreign_keys = ON;", args: []}], false, () => {
-      console.log('foreign keys turned on');
-    })
-    db.transaction((tx) => {
-      tx.executeSql("CREATE TABLE IF NOT EXIST users (id INT PRIMARY KEY, name VARCHAR(250) NOT NULL);", [], () => {console.log('table users created'), (e) => {console.warn('table users error :', e)}});
-    })
-  }, []);
+  const [location, setLocation] = useState(null);
+  const [errorMsg, setErrorMsg] = useState(null);
 
-  useEffect(() => {
-    db.transaction((tx) => {
-      tx.executeSql("SELECT * FROM users;");
-    })
-  }, []);
+  async function getUserLocation() {
+      const {status} = await Location.requestForegroundPermissionsAsync();
+      if (status !== "granted") {
+        setErrorMsg(`La permission d'accès a été réfusée`);
+        return;
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+      setLocation(location);
+  }
+
+  let text = 'en attente de réception...';
+  if (errorMsg) {
+    text = errorMsg;
+  }else if (location) {
+    text = JSON.stringify(location, null, 2);
+  }
 
   return(
       <View style={styles.container}>
-       <Text>{JSON.stringify(table)}</Text>
+        <Button title="Récupérer la position" onPress={getUserLocation} />
+        <Text>{text}</Text>
       </View>
   )
-
- 
 }
